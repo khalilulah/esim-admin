83a355f25e67580219ec6e776c71830e3b54cea6
khalilulah <alausakhalil@gmail.com>
2026-04-02 10:38:18 +0100

    removed unused react imports

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getProducts } from "../services/product.service";
 import { getOrders } from "../services/order.service";
 import type { Order } from "../types/index";
